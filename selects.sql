f19d22ee8d74e31855894e7e923656b343869952
ericbehughes <e.hughes92@icloud.com>
2017-04-24 13:53:36 -0400

    initial commit for selects

---
+++ selects.sql
@@ -0,0 +1,30 @@
+--carriers view the mailroute and truck are assigned to
+select mailrouteid, vehicleid, from carriers
+join mailroutes using(mailrouteid) 
+join vehicles using(vehicleid)
+
+/*
+Carriers should be able to see the list of mail  
+All the mail they must deliver, 
+all the mail to be delivered in a specific postal code that is part of their route, 
+all the mail addressed to a specific building that is part of their route.
+*/
+--all mail they must deliver
+select carrierid, routeid, mailid from carriers 
+join mailroutes using(routeid)
+join mail using(routeid)
+
+--all mail specific postal code
+select carrierid, routeid, mailid from carriers 
+join mailroutes using(routeid)
+join mail using(routeid) where postalcodeid = 'j4g1h3';
+
+--all mail to a specific buildiling 
+select carrierid, routeid, mailid from carriers 
+join mailroutes using(routeid)
+join mail using(routeid) where deliveryaddress = '123 fake street';
+
+
+select
+
+
