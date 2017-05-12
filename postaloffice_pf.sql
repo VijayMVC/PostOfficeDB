1333ca373fa6bd98faac64c8abb39d5111ef4926
ericbehughes <e.hughes92@icloud.com>
2017-05-11 22:03:06 -0400

    updated postaloffice_pf.sql

--- postaloffice_pf.sql
+++ postaloffice_pf.sql
@@ -1,33 +1,147 @@
-CREATE OR REPLACE PACKAGE types
-AS
-    TYPE ref_cursor IS REF CURSOR;
-END;
+--carriers must be able to mark themselves as unavailable
+ UPDATE employees set availability = 'availabiilitystatusfromuser'
+ where employeeid = 'his/hers employee id';
 
--- get mail route and truck they are assigned to
-CREATE OR REPLACE PROCEDURE GetRouteAndTruck(cID in varchar2) as
---variables
-  rID varchar2(64);
-  vID varchar2(64);
-  
+
+
+
+CREATE OR REPLACE PROCEDURE MarkRouteStatus(cID in varchar2,rID in varchar2, status IN varchar2) as
+ tempRouteID varchar2(64);
 BEGIN
---carriers view the mailroute and truck are assigned to
-  select routeid, vehicleId into rID, vID from carrier
-  join vehicles using(vehicleId)
-  where employeeId = cId;
-  --prints
- dbms_output.put_line('Carrier '||cID);
- dbms_output.put_line('RouteID'||rID);
- dbms_output.put_line('VehicleID  '||vID);
+--update mark a route as started 
+select routeid into tempRouteID from carrier 
+join routes using(routeid)
+where routeid = rID
+and employeeid = cID;
+
+if rID = tempRouteID then
+  update routes set routestatus = status where routeid = tempRouteID;
+end if;
 EXCEPTION
    WHEN NO_DATA_FOUND then 
-    	dbms_output.put_line('No Mail for this address'||address);
+    	dbms_output.put_line('No route updated '||rID);
 END;
-execute GetRouteAndTruck('01835 Eagle Crest Parkway');
+		
+Execute MarkRouteStatus('CA1','R3','Started') ;
 
- select routeid, vehicleId from carrier
+
+
+--mark each individual parcel or letter as delivered
+CREATE OR REPLACE PROCEDURE MarkMailStatus(cID in varchar2,mId in varchar2, status IN varchar2) as
+  tempdeliverystatus varchar2(64);
+ tempMailId varchar2(64);
+BEGIN
+--get parcel with mID
+    select mailid into tempMailId from carrier
+    join routes using(routeid)
+    join postalcode using(routeid)
+    join mail using(postalcode)
+    where employeeid = cID
+    and mailid = mID;
+    -- update mail status for this particular parcel
+    if tempMailId = mId then
+      update mail set deliverystatus = status where mailid = tempMailID;
+    end if;
+EXCEPTION
+   WHEN NO_DATA_FOUND then 
+    	dbms_output.put_line('No Mail for you to deliver '||cID);
+END;
+		
+Execute MarkMailStatus('CA1','M12', 'delivered') ;
+
+--get mail to delivery to specific building
+CREATE OR REPLACE PROCEDURE GetMailToDeliverByAddress(cID in varchar2,address in varchar2,csr OUT SYS_REFCURSOR) as  
+BEGIN
+  open csr for
+  select routeid, mailid, deliveryaddress from carrier 
+join Postalcode using(routeid)
+join mail using (postalcode)
+where employeeid = cID and deliveryaddress = address;
+EXCEPTION
+   WHEN NO_DATA_FOUND then 
+    	dbms_output.put_line('No Mail for you to deliver '||cID);
+END;
+create or replace function GetMailToDeliverByAddressTest(cID IN varchar, address in varchar) RETURN sys_refcursor
+ as
+ rc sys_refcursor;
+ begin
+ GetMailToDeliverByAddress(cID,address, rc);
+ return rc;
+ end;		
+select GetMailToDeliverByAddressTest('CA1','79 Trailsway Circle') from dual;
+
+
+--get mail to deliver by postal code
+CREATE OR REPLACE PROCEDURE GetMailToDeliverByPCode(cID in varchar2,pCode in varchar2,csr OUT SYS_REFCURSOR) as  
+BEGIN
+  open csr for
+  select routeid, mailid, deliveryaddress from carrier 
+  join postalcode using(routeid)
+  join mail using(postalcode)
+where employeeid = cId and postalcode = pCode;
+EXCEPTION
+   WHEN NO_DATA_FOUND then 
+    	dbms_output.put_line('No Mail for you to deliver '||cID);
+END;
+create or replace function GetMailToDeliverByPCodeTest(cID IN varchar, pCode in varchar) RETURN sys_refcursor
+ as
+ rc sys_refcursor;
+ begin
+ GetMailToDeliverByPCode(cID,pCode, rc);
+ return rc;
+ end;		
+select GetMailToDeliverByPCodeTest('CA1','H7T 3R4') from dual;
+
+
+-- get mail route and truck they are assigned to
+CREATE OR REPLACE PROCEDURE GetMailToDeliver(cID in varchar2,csr OUT SYS_REFCURSOR) as  
+BEGIN
+	dbms_output.put_line(cID);
+--carriers view the mailroute and truck are assigned to
+  open csr for
+  select employeeid, routeid, mailid from carrier
+  join postalcode using(routeid)
+  join mail using (postalcode)
+  where employeeid = cID
+  and deliverystatus is null;
+EXCEPTION
+   WHEN NO_DATA_FOUND then 
+    	dbms_output.put_line('No Mail for you to deliver '||cID);
+END;
+create or replace function GetMailToDeliverTest(cID IN varchar) RETURN sys_refcursor
+ as
+ rc sys_refcursor;
+ begin
+ GetMailToDeliver(cID,rc);
+ return rc;
+ end;		
+select GetMailToDeliverTest('CA1') from dual;
+
+
+
+
+
+-- get mail route and truck they are assigned to
+CREATE OR REPLACE PROCEDURE GetRouteAndTruck(cID in varchar2,csr OUT SYS_REFCURSOR) as  
+BEGIN
+	dbms_output.put_line(cID);
+--carriers view the mailroute and truck are assigned to
+  open csr for
+  select routeid, vehicleId from carrier
   join vehicles using(vehicleId)
-  where employeeid = 'CA1';
-
+  where employeeId = cID;
+EXCEPTION
+   WHEN NO_DATA_FOUND then 
+    	dbms_output.put_line('No Route or Vehicle info for Carrier '||cID);
+END;
+create or replace function GetRouteAndTruckTest(cID IN varchar) RETURN sys_refcursor
+ as
+ rc sys_refcursor;
+ begin
+ GetRouteAndTruck(cID,rc);
+ return rc;
+ end;		
+select GetRouteAndTruckTest('CA1') from dual;
 
 /* Find all mail postal code, */
 CREATE OR REPLACE PROCEDURE GetMailByAddress2(address in varchar2, csr OUT SYS_REFCURSOR) AS
@@ -78,21 +192,20 @@ select GetMailByPostalCodeTest('H4J 6S7') from dual;
 
 
 /* Find all mail based on route,*/
-CREATE OR REPLACE PROCEDURE GetMailByRouteID(rID in varchar2, OUT csr SYS_REFCURSOR) as  
+CREATE OR REPLACE PROCEDURE GetCarrierMailByRoute(rID in varchar2, csr OUT SYS_REFCURSOR) as  
 BEGIN
 --carriers view the mailroute and truck are assigned to
 dbms_output.put_line('ALL mail for route '||rID);
  open csr FOR
-  select mailid, postalcode, address, weight from routes
+  select mailid, postalcode, deliveryaddress, weight from routes
   Join postalcode using(routeid)
   join mail using(postalcode)
   where routeid = rID;
-   close csr;
 EXCEPTION
    WHEN NO_DATA_FOUND then 
     	dbms_output.put_line('No Mail for this route'||rID);
 END;
-execute GetMailByRoute('R1');
+execute GetCarrierMailByRoute('R1');
 
 create or replace function GetMailByRouteIDTest(pCode IN varchar) RETURN sys_refcursor
  as
@@ -103,7 +216,7 @@ create or replace function GetMailByRouteIDTest(pCode IN varchar) RETURN sys_ref
  end;		
 select GetMailByRouteIDTest('R1') from dual;
 
-CREATE or REPLACE FUNCTION FindReplacement(empId IN varchar2) RETURN varchar2
+CREATE or REPLACE Procedure FindReplacement(empId IN varchar2)
 AS
     TYPE varchar64_va IS VARRAY(64) OF VARCHAR2(64);
     employeeIds varchar64_va; -- array of isbns for each book as varchar
@@ -112,27 +225,25 @@ AS
     tempRouteId varchar2(64);
 BEGIN
   select employeeid bulk collect into employeeIds from employees 
-  where availability = 'available';
+  where availability = 'AVAILABLE';
   
   if employeeIds.COUNT > 1 then
     --update employee that is calling in sick
-    dbms_output.put_line(employeeIds(1) ||'found from select');
-       if substr(empId, 1,2) = 'CA'then
-       dbms_output.put_line(empId || 'input');
+       if substr(empId, 1,2) = 'CA' then
        select routeid into tempRouteId from carrier where employeeid = empId;
-       dbms_output.put_line(tempRouteId);
-       UPDATE carrier set routeid = tempRouteId where employeeId = empId;
-       UpdateAvailability(empId, 'unavailable');
+       UPDATE carrier set routeid = tempRouteId where employeeId = employeeIds(1);
+       UPDATE carrier set routeid = null where employeeId = empId;
+       UpdateAvailability(employeeIds(1), 'ONDUTY');
+       UpdateAvailability(empId, 'UNAVAILABLE');
       end if;
+      else
+      UpdateAvailability(employeeIds(1), 'ONDUTY');
+       UpdateAvailability(empId, 'UNAVAILABLE');
   end if;
-  RETURN tempRouteId;
 END;
-DECLARE
-		replacement varchar2(64);
-BEGIN
-		replacement := FindReplacement('CA3');
-		dbms_output.put_line(replacement);
-END;
+
+		execute FindCarrierReplacement('CA3');
+    select substr('CA3', 1,2) from dual;
 
 create or replace procedure UpdateAvailability(empId in varchar2, ablty in varchar2) as
 --variables
@@ -140,8 +251,10 @@ emp varchar2(64);
 BEGIN
 
 SELECT employeeid INTO emp from employees where employeeid = empid;
+  if emp is not null then
        UPDATE employees set availability = ablty
         where employeeid = emp;
+  end if;
 EXCEPTION
    WHEN NO_DATA_FOUND then 
     	dbms_output.put_line('UpdateAvailability - no employee with employee number '||empId);
